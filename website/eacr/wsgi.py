5c79a3e59ab406d20c7f2cb52039161b54e65f81
Daniel Hnyk <hnykda@gmail.com>
2017-03-14 18:16:28 +0100

    making gunicorn work

--- website/eacr/wsgi.py
+++ website/eacr/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eacr.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eacr.settings.base")
 
 application = get_wsgi_application()
