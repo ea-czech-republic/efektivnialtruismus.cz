05280b07e7325201dbcb1796117d2a4085575152
Daniel Hnyk <hnykda@gmail.com>
2017-03-21 12:20:07 +0100

    some tweaks for multiple envs

--- bin/gunicorn-start.sh
+++ bin/gunicorn-start.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
+set -e
 # run this from the git repo root dir
 
 NAME="efektivni-altruismus.cz"
 DJANGODIR=website
-SOCKFILE=../run/gunicorn.sock  # one level upper...
+SOCKFILE=../run/$1-gunicorn.sock  # one level upper...
 NUM_WORKERS=3
 DJANGO_WSGI_MODULE=eacr.wsgi
 
 [[ -z $EA_USER ]] && echo "You must pass EA_USER var" && exit 1
 [[ -z $EA_GROUP ]] && echo "You must pass EA_GROUP var" && exit 1
+[[ -z $1 ]] && echo "You must pass first parameter specifying environment" && exit 1
+[[ $DB_NAME != "$1-eacr.sqlite3" ]] && echo "DB name: $DB_NAME is not equal to environment passed as argument $1-eacr.sqlite3" && exit 1
 
-echo "Starting $NAME as $EA_USER:$EA_GROUP"
+echo "Starting $NAME as $EA_USER:$EA_GROUP in $1 environment and database $DB_NAME"
 
 # Activate the virtual environment
 source .venv/bin/activate
