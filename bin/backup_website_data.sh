5a7d32386dfd9e443a6f7d304e190d434bcf2ae9
hnykda <hnykda@gmail.com>
2017-03-21 23:05:07 +0100

    updating dev script init

--- bin/backup_website_data.sh
+++ bin/backup_website_data.sh
@@ -9,12 +9,3 @@ tar -cjSf $FILENAME bin/ website/ /etc/nginx/sites-available/beta-efektivni-altr
 $HOME/.bin/gdrive -c $HOME/.gdrive --service-account eacr-gdrive-creds.json upload -p 0BwgpG_Gt2nWnTG4zQ1NlYnI2VTA $FILENAME
 
 rm -rf $FILENAME
-
-
-# I had to create new project in google console, enable API for google drive and export credentials in JSON
-# then put these in ~/.config, build gdrive by `go get github.com/prasmussen/gdrive` and then use as is above
-
-# one more step was to create directory
-
-# To list backups 
-# gdrive --service-account eacr-gdrive-creds.json list --query " '0BwgpG_Gt2nWnTG4zQ1NlYnI2VTA' in parents"
--- bin/init-run.sh
+++ bin/init-run.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 set -e
+set -a
 
-source bin/development-env.sh
-/usr/bin/python -m venv .venv --copies
-source .venv/bin/activate
-pip install -r requirements.txt
+source bin/development-env.sh  # source a file with environment variables
+
+### Prepare Python environment
+python3 -m venv .venv --copies  # create a virtual environment
+source .venv/bin/activate  # activate the environment
+pip install -r requirements.txt  # install all dependencies
+
+# Prepare wagtail app
 cd website
-python manage.py makemigrations
-python manage.py migrate 
-python manage.py createsuperuser
-python manage.py runserver
+python3 manage.py makemigrations
+python3 manage.py migrate  # prepare database for all models
+python3 manage.py createsuperuser 
+python3 manage.py runserver  # runserver on 127.0.0.1:8000
+set +a
