90937cbcf9b34cf51accc19147cdafd989e19ea6
hnykda <hnykda@gmail.com>
2017-09-28 09:43:18 +0200

    finalization

--- website/theses/views.py
+++ website/theses/views.py
@@ -9,12 +9,12 @@ THESES_MAILS = ['theses@efektivni-altruismus.cz']
 
 def build_mail_content(uri, data):
     return dedent("""
-    URL: {thesis_uri}
-    Contact email: {contact_email},
+    URL: {uri}
+    Contact email: {contact_email}
 
     --------Message--------
     {content}
-    """.format(thesis_uri=uri, **data))
+    """.format(uri=uri, **data))
 
 
 def feedback_form(request):
@@ -26,16 +26,16 @@ def feedback_form(request):
             contact_email = form.cleaned_data['contact_email']
             if contact_email is None:
                 contact_email = THESES_MAILS[0]
-
-            mail_content = build_mail_content(request.build_absolute_uri(), form.cleaned_data)
-            send_mail('Feedback',
+            uri = request.build_absolute_uri()
+            mail_content = build_mail_content(uri,
+                                              form.cleaned_data)
+            send_mail('Feedback on page {}'.format(uri),
                       mail_content,
                       THESES_MAILS,
                       contact_email
                       )
-            #return JsonResponse({'message': 'Thank you for your interest! '
-            #                    'We will let get back to you soon!'})
-            return redirect(request.META['HTTP_REFERER'])
+            return JsonResponse({'message': 'Thank you for your interest! '
+                                            'We will let get back to you soon!'})
         else:
             return JsonResponse({'message': 'Sorry, submitting your form was not '
                                             'successful. Please use our contact page.'})
