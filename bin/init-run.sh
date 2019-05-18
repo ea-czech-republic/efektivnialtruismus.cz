97129b765490e2a3fb69f5c268f5c166d7987c34
Daniel Hnyk <hnykda@gmail.com>
2019-05-18 12:14:36 +0200

    remove old scripts

--- bin/init-run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-### Prepare Python environment
-python3 -m venv .venv --copies  # create a virtual environment
-source .venv/bin/activate  # activate the environment
-pip install -r requirements.txt  # install all dependencies
