f7129e56d99159d2c0ba6fc289590b81623186d7
Daniel Hnyk <hnykda@gmail.com>
2019-07-06 18:54:48 +0200

    adding ask as anything

--- website/theses/urls.py
+++ website/theses/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import conversion, submit_to_newsletter
+from .views import conversion, submit_to_newsletter, ask_as_anything
 
 theses_urls = [
     url(r"^conversion/$", conversion, name="conversion"),
-    url(r"^newsletter/$", submit_to_newsletter, name="newsletter")
+    url(r"^newsletter/$", submit_to_newsletter, name="newsletter"),
+    url(r"^ask_us_anything/$", ask_as_anything, name="ask_us_anything"),
 ]
--- website/theses/views.py
+++ website/theses/views.py
@@ -6,6 +6,7 @@ from textwrap import dedent
 
 THESES_MAILS = ["david@effectivethesis.com"]
 
+
 def conversion(request, origin_url, title):
     context = {"origin_url": origin_url, "title": title}
     return render(request, "theses/conversion.html", context)
@@ -14,32 +15,57 @@ def conversion(request, origin_url, title):
 def coaching_conversion(request):
     return render(request, "theses/coaching_conversion.html")
 
-def build_mail_content_contact(contact_name, contact_email):
+
+def build_mail_content_contact(contact_name, contact_email, message):
     return dedent(
         f"""
     Name: {contact_name},
     Contact email: {contact_email},
 
     --------Message--------
-    Interest in newsletter
+    {message}
     """)
+
+
+def dejsonize(content):
+    return {x['name']: x['value']
+            for x in json.loads(content)
+            }
+
 
 @csrf_exempt
 def submit_to_newsletter(request):
+    print(request.is_ajax())
     if request.is_ajax():
         if request.method == 'POST':
-            form_data = {x['name']: x['value']
-                for x in json.loads(request.body.decode())
-            }
+            form_data = dejsonize(request.body.decode())
             contact_name = form_data['contact_name']
             contact_email = form_data['contact_email']
             send_mail(
-                "Contacting using Contact Form",
-                build_mail_content_contact(contact_name, contact_email),
+                "Contacting using Contact Form in Footer",
+                build_mail_content_contact(contact_name, contact_email, "Interest in newsletter"),
                 THESES_MAILS,  # recipient email
                 contact_email,
             )
-            print(form_data)
             return HttpResponse("Thank you for your interest!")
     else:
         return HttpResponse("Something went wrong when submitting to newsletter")
+
+
+@csrf_exempt
+def ask_as_anything(request):
+    if request.is_ajax():
+        if request.method == 'POST':
+            form_data = dejsonize(request.body.decode())
+            contact_name = form_data['contact_name']
+            contact_email = form_data['contact_email']
+            text = form_data['contact_email']
+            send_mail(
+                "Contacting using Contact Form on About us",
+                build_mail_content_contact(contact_name, contact_email, text),
+                THESES_MAILS,  # recipient email
+                contact_email,
+            )
+            return HttpResponse("Thank you for your question! We'll get back to you as soon as possible.")
+    else:
+        return HttpResponse("Something went wrong when contacting us.")
