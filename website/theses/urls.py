5e7a1fc852c2e30168a7b73c9e3210993c65b4bc
Daniel Hnyk <hnykda@gmail.com>
2019-07-24 20:07:10 +0200

    beta

--- website/theses/urls.py
+++ website/theses/urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls import url
-from .views import conversion
+from .views import conversion, submit_to_newsletter, ask_us_anything
 
-theses_urls = [url(r"^conversion/$", conversion, name="conversion")]
+theses_urls = [
+    url(r"^conversion/$", conversion, name="conversion"),
+    url(r"^newsletter/$", submit_to_newsletter, name="newsletter"),
+    url(r"^ask_us_anything/$", ask_us_anything, name="ask_us_anything"),
+]
