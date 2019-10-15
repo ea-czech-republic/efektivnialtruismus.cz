29a0725f4c615fc6bb91ac09a0c36d9a30eb3a05
Daniel Hnyk <hnykda@gmail.com>
2019-10-15 23:33:05 +0200

    add coments

--- bin/init-letsencrypt.sh
+++ bin/init-letsencrypt.sh
@@ -2,6 +2,8 @@
 
 cd /var/server/efektivnialtruismus.cz
 
+# You may need to setup IMAGE_TAG and ENVIRONMENT to make it work
+
 docker_compose="docker run --rm \
     -e IMAGE_TAG=$IMAGE_TAG \
     -e ENVIRONMENT=$ENVIRONMENT \
@@ -21,7 +23,7 @@ domains=(effectivethesis.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="martin.racak@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
