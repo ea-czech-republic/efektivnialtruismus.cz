654e5b3c2d185d191032a55bdc03c95ec082e2f6
Daniel Hnyk <hnykda@gmail.com>
2019-12-20 20:56:12 +0100

    fix envs with certificates

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,15 @@ set -ex
 [[ -z $IMAGE_TAG ]] && echo "You must pass IMAGE_TAG var" && exit 1
 [[ -z ENVIRONMENT ]] && echo "You must pass ENVIRONMENT var which corresponds to a branch" && exit 1
 
+if [ "$ENVIRONMENT" = "beta" ]; then
+    export ENV_PREFIX="beta."
+elif [ "$ENVIRONMENT" = "master" ]; then
+    export ENV_PREFIX=""
+else
+    echo "Unknown env"
+    exit 1
+fi
+
 # this was done by git clone https://github.com/ea-czech-republic/efektivnialtruismus.cz.git
 cd /var/server/efektivnialtruismus.cz
 git checkout $ENVIRONMENT
@@ -15,6 +24,7 @@ docker pull czea/effective-thesis:${IMAGE_TAG}
 docker run --rm \
     -e IMAGE_TAG=$IMAGE_TAG \
     -e ENVIRONMENT=$ENVIRONMENT \
+    -e ENV_PREFIX=$ENV_PREFIX \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$PWD:$PWD" \
     -w="$PWD" \
