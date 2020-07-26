a845f1dc3ba6ff8c99bf2ccd26e8330eb4718f6b
Vojtech Vesely <vesely.v4@gmail.com>
2020-07-26 18:27:28 +0200

    sitemap added

--- website/eacr/settings.py
+++ website/eacr/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sitemaps",
     "django.contrib.sites",
     "disqus",
     "wagtailmenus",
