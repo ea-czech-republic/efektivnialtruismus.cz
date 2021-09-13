f729cd8324e3c2600cba4df27f560fb45c268f66
Vojtech Vesely <vesely.v4@gmail.com>
2021-09-13 19:58:21 +0200

    Adjusting beta configs

--- bin/init-letsencrypt.sh
+++ bin/init-letsencrypt.sh
@@ -19,7 +19,7 @@ if ! [ -x "$(command -v $docker_compose)" ]; then
   exit 1
 fi
 
-domains=(effectivethesis.org effectivethesis.com)
+domains=(effectivethesis.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="martin.racak@gmail.com" # Adding a valid address is strongly recommended
@@ -36,8 +36,8 @@ fi
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
   echo
 fi
 
